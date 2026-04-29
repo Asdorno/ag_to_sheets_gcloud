d53e8ad1d32e326de67fe6ccb8abfc78095ebb71
Asdorno <fedishev0312@gmail.com>
2026-04-29 11:54:50 +0200

    Added get_project_id() method, so Google Cloud project's id is not hardcoded

--- secret_accessor.py
+++ secret_accessor.py
@@ -1,4 +1,32 @@
+import os
+from dotenv import load_dotenv
 from google.cloud import secretmanager
+import google.auth
+
+
+def get_project_id() -> str:
+    """
+        Retrieves the Google Cloud project ID.
+
+        Returns:
+            str: the Google Cloud project ID.
+
+        Raises:
+            RuntimeError: If Google Cloud project ID is not accessible through default auth or environment variables.
+        """
+    load_dotenv()
+    _, project_id = google.auth.default()
+
+    if project_id:
+        return project_id
+
+    project_id = os.getenv("GOOGLE_CLOUD_PROJECT")
+    if not project_id:
+        raise RuntimeError(
+            "Could not determine Google Cloud project ID."
+            "Set GOOGLE_CLOUD_PROJECT environment variable or configure Application Default Credentials"
+        )
+    return project_id
 
 
 def get_secret(secret_id):
@@ -12,6 +40,7 @@ def get_secret(secret_id):
         str: The decoded secret value.
     """
     client = secretmanager.SecretManagerServiceClient()
-    name = f"projects/whats-app-project-toni-wihbe/secrets/{secret_id}/versions/latest"
+    project_id = get_project_id()
+    name = f"projects/{project_id}/secrets/{secret_id}/versions/latest"
     response = client.access_secret_version(request={"name": name})
     return response.payload.data.decode('UTF-8')
